37273b749171e60afcfc9188a6483788daa37567
geisalopes <72300375+geisalopes@users.noreply.github.com>
2023-12-07 14:14:50 +0100

    Add rel="noreferrer" to the links with target="_blank"

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://geisalopes.github.io/" target="_blank">
+        <a
+          href="https://geisalopes.github.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Geisa Lopes
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/geisalopes/react-weather-app"
           target="_blank"
+          rel="noopener noreferrer"
         >
           open-sourced on Github
         </a>
