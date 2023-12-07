1fa57130924aa1825bc3ad91b082a0414f5f36b9
geisalopes <72300375+geisalopes@users.noreply.github.com>
2023-12-07 13:54:19 +0100

    Add GitHub repository and personal website URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://geisalopes.github.io/" target="_blank">
+          Geisa Lopes
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/geisalopes/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
