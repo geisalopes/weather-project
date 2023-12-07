7676f119c12a10bb0ef828197973a48a96c5d822
geisalopes <72300375+geisalopes@users.noreply.github.com>
2023-12-07 17:19:12 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://geisalopes.github.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Geisa Lopes
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/geisalopes/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://geisalopes.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Geisa Lopes
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/geisalopes/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
