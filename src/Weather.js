99e5ac9d40e96b80995bf90c968c437979275fbe
geisalopes <72300375+geisalopes@users.noreply.github.com>
2023-12-08 10:19:08 +0100

    Add basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="sumbit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,19 @@ export default function Weather() {
         <li>Thursday 17:32</li>
         <li>Fog</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/fog.png"
-            alt="Fog"
-          ></img>
-          6ºC
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/fog.png"
+              alt="Fog"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">ºC</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
