20a1889bbc46f0a81a257e68b748b6fe6704f338
geisalopes <72300375+geisalopes@users.noreply.github.com>
2023-12-12 14:18:22 +0100

    Update the icon url

--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ export default function Weather(props) {
       ready: true,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: `https://ssl.gstatic.com/onebox/weather/64/fog.png`,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
